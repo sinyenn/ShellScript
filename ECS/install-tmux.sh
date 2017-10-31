04a861d8dcdcb7f999ed4360f8ae7b5d553b3024
sinyenn <sinyenn@gmail.com>
2017-10-31 13:49:08 +0800

    upgrade libevent to 2.1.8

--- ECS/install-tmux.sh
+++ ECS/install-tmux.sh
@@ -16,14 +16,18 @@ checkResult()
     fi
 }
 
+#depends
+yum install gcc git automake -y
+checkResult
+
 #tmux depends on libevent 2.x.
-wget https://github.com/libevent/libevent/releases/download/release-2.0.22-stable/libevent-2.0.22-stable.tar.gz
+wget https://github.com/libevent/libevent/releases/download/release-2.1.8-stable/libevent-2.1.8-stable.tar.gz
 checkResult
-tar -zxvf libevent-2.0.22-stable.tar.gz
+tar -zxvf libevent-2.1.8-stable.tar.gz
 checkResult
-rm -rf libevent-2.0.22-stable.tar.gz
+rm -rf libevent-2.1.8-stable.tar.gz
 checkResult
-cd libevent-2.0.22-stable
+cd libevent-2.1.8-stable
 checkResult
 ./configure && make
 checkResult
